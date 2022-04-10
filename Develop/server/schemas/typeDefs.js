58243300bc646dc5ef00386c37db76523c74ee67
Tiffany <tiff.sobczak@gmail.com>
2022-04-09 20:00:17 -0500

    finished save and remove book i resolvers

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,4 +1,40 @@
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
+const { gql } = require('apollo-server-express');
 
-module.exports = { typeDefs, resolvers };
+
+const typeDefs = gql `
+
+type Query{
+    me: User
+}
+
+type Mutation {
+    login: (email: String, password: String): Auth
+    addUser: (username: string, email: string, password: String): Auth
+    saveBook:  (authors: [String], description: String, title: String, bookId: String, image: ?????, link: String)
+}
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: ?????????
+    link: 
+  }
+
+  type Auth {
+    token: ID! 
+    user: User
+  }
+
+`;
+
+module.exports = typeDefs;
